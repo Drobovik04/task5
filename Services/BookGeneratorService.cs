aacc8fab5b4b89900d7689553c78e22880fff869
Drobovik04 <misle92@mail.ru>
2024-11-19 22:28:29 +0300

    replace bad lorem generator on addresses

--- Services/BookGeneratorService.cs
+++ Services/BookGeneratorService.cs
@@ -13,7 +13,7 @@ namespace task5.Services
             var books = new List<Book>();
             faker.RuleFor(o => o.Index, s => batch * batchSize + startIndex++)
                 .RuleFor(o => o.ISBN, s => s.Random.Replace("###-#-###-#####-#"))
-                .RuleFor(o => o.Title, s => s.Lorem.Sentence())
+                .RuleFor(o => o.Title, s => s.Address.FullAddress())
                 .RuleFor(o => o.Author, s => s.Name.FullName())
                 .RuleFor(o => o.Publisher, s => s.Company.CompanyName() + ", " + s.Date.BetweenDateOnly(new DateOnly(1900, 1, 1), new DateOnly(2024, 11, 1)).Year)
                 .RuleFor(o => o.Likes, s => settings.AvgLikes % 1 != 0 && s.Random.Double(0, 1) <= settings.AvgLikes % 1 ? (int)settings.AvgLikes/1 + 1 : (int)settings.AvgLikes/1)
@@ -31,7 +31,7 @@ namespace task5.Services
 
             for (int i = 0; i < reviewsCount; i++)
             {
-                reviews.Add(new Review() { Reviewer = faker.Name.FullName(), ReviewText = faker.Lorem.Paragraph()});
+                reviews.Add(new Review() { Reviewer = faker.Name.FullName(), ReviewText = faker.Address.FullAddress()});
             }
 
             return reviews;
