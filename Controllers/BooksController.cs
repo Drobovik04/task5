ee012afbe9560cafddcd7e463909f25607c3cea4
Drobovik04 <misle92@mail.ru>
2024-11-19 20:01:16 +0300

    v1

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,6 +2,10 @@
 using task5.Models;
 using task5.Services;
 using System.Text.Json.Serialization;
+using System.Formats.Asn1;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace task5.Controllers
 {
@@ -24,6 +28,25 @@ namespace task5.Controllers
         {
             var books = _bookGenerator.GenerateBooks(settings, batch, 20, startIndex);
             return Json(books);
+        }
+        public IActionResult ExportToCsv([FromBody] Settings settings, int totalbatches)
+        {
+            var allBooks = new List<Book>();
+            for (int batch = 0; batch < totalbatches; batch++)
+            {
+                var books = _bookGenerator.GenerateBooks(settings, batch, 20, 0);
+                allBooks.AddRange(books);
+            }
+
+            var memoryStream = new MemoryStream();
+            var writer = new StreamWriter(memoryStream);
+            var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+            csv.WriteRecords(allBooks); 
+            writer.Flush();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return File(memoryStream, "text/csv", "books.csv");
         }
     }
 }
--- Services/BookGeneratorService.cs
+++ Services/BookGeneratorService.cs
@@ -9,13 +9,13 @@ namespace task5.Services
     {
         public List<Book> GenerateBooks(Settings settings, int batch, int batchSize, int startIndex)
         {
-            var faker = new Faker<Book>(settings.Language).UseSeed(settings.Seed + batch);
+            var faker = new Faker<Book>(locale: settings.Language).UseSeed(settings.Seed + batch);
             var books = new List<Book>();
             faker.RuleFor(o => o.Index, s => batch * batchSize + startIndex++)
                 .RuleFor(o => o.ISBN, s => s.Random.Replace("###-#-###-#####-#"))
                 .RuleFor(o => o.Title, s => s.Lorem.Sentence())
                 .RuleFor(o => o.Author, s => s.Name.FullName())
-                .RuleFor(o => o.Publisher, s => s.Company.CompanyName())
+                .RuleFor(o => o.Publisher, s => s.Company.CompanyName() + ", " + s.Date.BetweenDateOnly(new DateOnly(1900, 1, 1), new DateOnly(2024, 11, 1)).Year)
                 .RuleFor(o => o.Likes, s => settings.AvgLikes % 1 != 0 && s.Random.Double(0, 1) <= settings.AvgLikes % 1 ? (int)settings.AvgLikes/1 + 1 : (int)settings.AvgLikes/1)
                 .RuleFor(o => o.CoverImage, s => s.Image.PicsumUrl())
                 .RuleFor(o => o.ReviewDetails, s => GenerateReviews(s, (settings.AvgReviews % 1 != 0 && s.Random.Double(0, 1) <= settings.AvgReviews % 1 ? (int)settings.AvgReviews/1 + 1 : (int)settings.AvgReviews/1)));
